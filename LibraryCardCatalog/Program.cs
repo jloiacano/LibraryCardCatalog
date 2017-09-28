7b0f4ef64df11ea9028f21e4fb5fec37129766c4
southclaw23 <mjsauro@gmail.com>
2017-09-28 15:20:58 -0500

    added multple classes
    add card catalog class, variables and a list

--- LibraryCardCatalog/Program.cs
+++ LibraryCardCatalog/Program.cs
@@ -10,15 +10,41 @@ namespace LibraryCardCatalog
     {
         static void Main(string[] args)
         {
+
         }
     }
 
-    class Book
+    public class Book
+    {
+
+        private string Title;
+        private string Author;
+        private string Isbn;
+        private string Publisher;
+
+        public Book(string title, string author, string isbn, string publisher)
         {
-        public Book(string title, string author)
+            Title = title;
+            Author = author;
+            Isbn = isbn;
+            Publisher = publisher;
+        }
+    }
+
+    public class CardCatalog
+    {
+        private string _filename;
+
+        public CardCatalog(string fileName)
         {
 
         }
+
+        List<Book> myBooks = new List<Book>();
+
+        //add book - enter title, author, isbn and publisher
+        //  after each book, we should save
+        //list book - retrieve the list of books and display them
     }
 
 }
