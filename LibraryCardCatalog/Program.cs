9ddda5343850ebf0d5c37a4918d6adedb300b7ff
southclaw23 <mjsauro@gmail.com>
2017-09-29 13:07:03 -0500

    got the menu working
    added the menu.  you can choose option 1 to add a book, option 2 to list all books and option 3 to exit.  we'll need to work on the methods to actually accomplish these.

--- LibraryCardCatalog/Program.cs
+++ LibraryCardCatalog/Program.cs
@@ -10,7 +10,74 @@ namespace LibraryCardCatalog
     {
         static void Main(string[] args)
         {
+            
+            int numInput = PromptForNumber("Choose Option 1 to add a book.  " +
+                "\nChoose Option 2 to list all books. " +
+                "\nChoose Option 3 to save and exit");
 
+            while (numInput <= 3)
+            {
+                if (numInput == 1)
+                {
+                    Console.WriteLine("You chose option 1. Add Book.");
+
+                    Console.WriteLine("Enter the book title: ");
+                    string bookTitle = Console.ReadLine();
+
+                    Console.WriteLine("Enter the author: ");
+                    string author = Console.ReadLine();
+
+                    Console.WriteLine("Enter the ISBN: ");
+                    string isbn = Console.ReadLine();
+
+                    Console.WriteLine("Enter the publisher: ");
+                    string publisher = Console.ReadLine();
+
+                    Book a = new Book(bookTitle, author, isbn, publisher);
+                    Console.WriteLine(bookTitle, author, isbn, publisher);
+
+                    //return to menu
+                    numInput = PromptForNumber("Choose Option 1 to add a book.  " +
+                "\nChoose Option 2 to list all books. " +
+                "\nChoose Option 3 to save and exit");
+                  
+                }
+                else if (numInput == 2)
+                {
+                    //call the list book method
+                    Console.WriteLine("You chose option 2");
+                    Console.ReadLine();
+
+                    //return to menu
+                    numInput = PromptForNumber("Choose Option 1 to add a book.  " +
+                "\nChoose Option 2 to list all books. " +
+                "\nChoose Option 3 to save and exit");
+                }
+                else
+                {
+                    //save and exit
+                    //to be removed later
+                    Console.WriteLine("You chose option 3");
+
+                    //exit
+                    return;
+                    
+                }
+            }
+        }
+
+        private static int PromptForNumber(string promptString = "Enter a number: ")
+        {
+            int num = 0;
+            string input = "";
+
+            while (!int.TryParse(input, out num) || (Convert.ToInt32(input) > 3))
+            {
+                Console.WriteLine(promptString);
+                input = Console.ReadLine();
+            }
+            
+            return num;
         }
     }
 
@@ -29,11 +96,26 @@ namespace LibraryCardCatalog
             Isbn = isbn;
             Publisher = publisher;
         }
+
+        //add book - enter title, author, isbn and publisher
+
+        //public string addBook(string title, string author, string isbn, string publisher)
+        //{
+        //    Title = title;
+        //    Author = author;
+        //    Isbn = isbn;
+        //    Publisher = publisher;
+            
+        //}
     }
 
     public class CardCatalog
     {
         private string _filename;
+        private string Title;
+        private string Author;
+        private string Isbn;
+        private string Publisher;
 
         public CardCatalog(string fileName)
         {
@@ -42,9 +124,11 @@ namespace LibraryCardCatalog
 
         List<Book> myBooks = new List<Book>();
 
-        //add book - enter title, author, isbn and publisher
         //  after each book, we should save
         //list book - retrieve the list of books and display them
+
+
     }
+
 
 }
